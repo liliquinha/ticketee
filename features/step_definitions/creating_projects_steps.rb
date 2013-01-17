01e9621f87ca229347461687b19a3fe5476ce9d7
liliquinha <stuti.lila@gmail.com>
2013-01-17 03:00:27 -0200

    added the ability to create users from admin backend

--- features/step_definitions/creating_projects_steps.rb
+++ features/step_definitions/creating_projects_steps.rb
@@ -21,3 +21,7 @@ end
 Then /^I should be on the project page for "(.*?)"$/ do |project_name|
   visit project_path(Project.find_by_name!(project_name))
 end
+
+When /^I check "(.*?)"$/ do |option|
+  check(option)
+end
